f2c4b08b47a884dd68f863aca5856fb690ec0012
sayakura <linra.kura@gmail.com>
2018-11-12 15:04:13 -0800

    a

---
+++ PilotCity/src/components/wizard/employer/w_sector.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="w_sector">
+        <div class="btn-group btn-group-lg" role="group">
+            <button type="button" class="btn btn-secondary">Left</button>
+            <button type="button" class="btn btn-secondary">Middle</button>
+            <button type="button" class="btn btn-secondary">Right</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "w_sector"
+}
+</script>
+
+<style>
+
+</style>
