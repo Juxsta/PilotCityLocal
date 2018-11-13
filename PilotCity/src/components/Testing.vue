ce2c200dc131cc418e756fd3a52bb8c8fd7d75d4
Juxsta <ericjrreyes2@gmail.com>
2018-11-13 00:13:00 -0800

    merge

---
+++ PilotCity/src/components/Testing.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+<script>
+import w_skills from '@/components/wizard/teacher/w_skills'
+export default {
+    name: 'Testing',
+    data: {
+        return {
+            
+        }
+    },
+    components:
+    {
+        w_skills
+    }
+}
+</script>
