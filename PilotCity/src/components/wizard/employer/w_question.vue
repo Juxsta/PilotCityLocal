90fbf344ba2a836ac95b200d3c37e5ccceca7f28
sayakura <linra.kura@gmail.com>
2018-11-12 16:02:39 -0800

    a

--- PilotCity/src/components/wizard/employer/w_question.vue
+++ PilotCity/src/components/wizard/employer/w_question.vue
@@ -1,6 +1,13 @@
 <template>
-    <div id="w_question">
-
+        <div id="w_question" class="container">
+            <form>
+                <h3 class="font-weight-light"> Enter Quesion </h3>
+                <input type="text" class="form-control mb-3" placeholder="State your challenge in the form of quesion here">
+                <div class="panel_tagspanel">
+                    <span class="badge badge-pill badge-primary">predefined quesion</span>
+                    <span class="badge badge-pill badge-primary">predefined quesion</span>
+            </div>
+            </form>
     </div>
 </template>
 
