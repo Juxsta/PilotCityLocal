b281e2ca10daaf05f7fbf2e303cec572a6a2ab04
Derick Lee <dericklee@Dericks-MacBook-Pro-2.local>
2018-11-14 03:54:52 -0800

    modal end

--- PilotCity/src/components/wizard/teacher/w_tymod.vue
+++ PilotCity/src/components/wizard/teacher/w_tymod.vue
@@ -10,13 +10,13 @@
         <div class="modal-dialog modal-dialog-centered" role="document" >
             <div class="modal-content pc-light-gray">
             <div class="d-flex flex-column modal-body">
-                    <h2 class="Raleway font-weight-bold ml-auto mr-auto">Thank you</h2>
+                    <h2 class="Raleway font-weight-bold ml-auto mr-auto text-thank-you">Thank you</h2>
                 <div class="ml-auto mr-auto">
-                    <img src='@/assets/2_Heart.png' alt="Heart Logo" id='Heartlogo' >
+                    <img src='@/assets/matchmake.png' alt="Matchmake" id='Matchmake' >
                 </div>
-                <h2 class="ml-auto mr-auto">We Will Notify you when your match results are available</h2>
+                    <h2 class="ml-auto mr-auto text-title-notify">We will notify you when your match results are available.</h2>
                 <div class="ml-auto mr-auto">
-                    <button class="btn btn-lg">Over and out</button>
+                    <button class="btn btn-lg button-over-out">Over and out</button>
                 </div>
             </div>
             </div>
@@ -42,5 +42,55 @@ export default {
 </script>
 
 <style>
+
+.text-title-notify {
+    font-family: Raleway;
+    color: #ffffff;
+    text-align: center;
+    font-weight: 700;
+    margin-bottom:25px;
+    margin-left:100px;
+    font-size: 30px;
+}
+
+.text-thank-you {
+    font-family: Raleway;
+    color: #eca0be;
+    text-align: center;
+    font-weight: 700;
+    margin-top:25px;
+}
+
+.button-over-out {
+    border-radius: 50px;
+    font-family: Raleway;
+    font-weight: 700;
+    font-size: 20px;
+    margin-bottom: 20px;
+    padding-left:30px;
+    padding-right:30px;
+    background:transparent;
+    border-color: #eca0be;
+    color:  #eca0be;
+}
+
+.button-over-out:hover {
+    border-radius: 50px;
+    font-family: Raleway;
+    font-weight: 700;
+    font-size: 20px;
+    margin-bottom: 20px;
+    padding-left:30px;
+    padding-right:30px;
+    background: #ffffff;
+    border-color: #eca0be;
+    color:  #eca0be;
+}
+
+#Matchmake {
+    text-align: center;
+    width:100px;
+    margin:20px;
+}
 
 </style>
