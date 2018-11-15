52ef9d83042e2099f6c86a295a214f19fab3d05b
Eric Reyes <Ericjrreyes2@gmail.com>
2018-11-15 11:40:32 -0800

    pull

--- PilotCity/src/components/wizard/employer/w_department.vue
+++ PilotCity/src/components/wizard/employer/w_department.vue
@@ -1,19 +1,50 @@
 <template>
-    <div id="w_department" class="container">
-        <div class="input-group">
-            <input  type="text" class="form-control" placeholder="department name">
-            <input type="number" class="form-control" placeholder="team size">
-            <input type="text" class="form-control" placeholder="team member">
+    <div id="w_department" class="container mt-5">
+        <div class="form-row" v-for="department in departments" :key=id>
+            <div class="col">
+                <label for="Department_Name">Department Name</label>
+                <input  type="text" class="form-control" placeholder="department name">
+            </div>
+            <div class="col">
+                <label for="Team_Size">Team Size</label>
+                <select class="custom-select" id="select_team">
+                    <option value="0">1 - 3</option>
+                    <option value="1">4 - 6</option>
+                    <option value="2">7 - 10</option>
+                    <option value="3">11 - 20</option>
+                    <option value="4">21 - 50</option>
+                    <option value="5">50+</option>
+                </select>
+            </div>
+            <div class="col">
+                <label for="team_member">Team Member</label>
+                <select class="custom-select" id="select_team_member">
+                    <option value=false>No</option>
+                    <option value=false>Yes</option>
+                </select>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "w_department"
+    name: "w_department",
+    data () {
+        return {
+            department: {
+                uid: new Date
+                name: null,
+                team_size: null,
+                team_member: null
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+#w_department {
+    
+}
 </style>
