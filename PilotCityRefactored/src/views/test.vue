bce8719b835c5263916658c9ddb3f09c2ed9cab4
Eric Reyes <Ericjrreyes2@gmail.com>
2018-12-05 20:55:34 -0800

    new updates

---
+++ PilotCityRefactored/src/views/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <matchmade />
+    </div>
+</template>
+
+<script>
+import matchmade from '../views/matchmade'
+export default {
+    name:'test',
+    components:{
+        matchmade
+    }
+}
+</script>
+
+
+
