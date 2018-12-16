d644a07db5c705fa2235e6a3a9948ac7842e8da7
sayakura <linra.kura@gmail.com>
2018-12-15 20:33:34 -0800

    a

--- PilotCityRefactored/src/components/map/GoogleMap.vue
+++ PilotCityRefactored/src/components/map/GoogleMap.vue
@@ -3,7 +3,8 @@
 </template>
 
 <script>
-import axios from 'axios';
+import * as firebase from 'firebase';
+import db from "@/firebase/init";
 export default {
   name: 'GoogleMap',
   props: ['name', 'map_data', 'apikey', 'mapcenter'],
@@ -12,7 +13,8 @@ export default {
           map: null,
           markers: null,
           markers_ref: null,
-          last_marker: null
+          last_marker: null,
+          e_marker: null
       }
   },
   watch:{
@@ -25,9 +27,19 @@ export default {
         this.last_marker.setIcon('https://firebasestorage.googleapis.com/v0/b/pilotcity-firestore.appspot.com/o/add-label-button.png?alt=media&token=5f3e240e-1b9d-41dc-92bb-aaf6baac24e2')
       this.markers_ref[i].marker.setIcon('https://firebasestorage.googleapis.com/v0/b/pilotcity-firestore.appspot.com/o/active_marker%20(1).png?alt=media&token=72a11cb1-c853-41d7-bcbe-a9e75aeb8104')
       this.last_marker = this.markers_ref[i].marker;
+    },
+    map_data: function(){
+      for (var i = 0; i < this.markers_ref.length; i++)
+        this.markers_ref[i].marker.setMap(null)
+      this.markers = [];
+      for (var i = 0; i < this.map_data.length; i++)
+        this.markers.push(this.map_data[i].coordinate);
+      this.pinAllClassroomsOnMap(this.map);
     }
   },
   mounted: function () {
+    var self = this;
+
     this.markers = [];
     for (var i = 0; i < this.map_data.length; i++)
       this.markers.push(this.map_data[i].coordinate);
@@ -42,6 +54,20 @@ export default {
     }
     this.map = new google.maps.Map(element, options);
     this.pinAllClassroomsOnMap(this.map);
+        db.firestore().collection('employers').doc(db.auth().currentUser.uid).get().then(res => {
+        console.log(db.auth().currentUser.uid)
+        var coordinate;
+        if (res.data())
+          coordinate = res.data().coordinate;
+        if (coordinate){
+          self.e_marker = new google.maps.Marker({  position: coordinate,
+                                        map: this.map,
+                                        icon: 'https://firebasestorage.googleapis.com/v0/b/pilotcity-firestore.appspot.com/o/active_marker.png?alt=media&token=369977bd-2457-4f4d-9f7f-2c9a0b60030b'
+                                      });
+        } else {
+          console.log("Looks like you are homeless ; (");
+        }
+    });
   },
    methods:{
     pinAllClassroomsOnMap(map){
@@ -49,7 +75,6 @@ export default {
         var pin_promises = [];
         var key;
         this.markers_ref = [];
-      
         for (var i = 0; i < this.markers.length; i++)
         {
           key = String(this.markers[i].lat) + String(this.markers[i].lng);
@@ -60,7 +85,7 @@ export default {
               marker: 
               new google.maps.Marker({  position: this.markers[i],
                                         map: map,
-                                        label: {text: i + "", color: "white"},
+                                        label: {text: (i + 1) + "", color: "white"},
                                         icon: 'https://firebasestorage.googleapis.com/v0/b/pilotcity-firestore.appspot.com/o/add-label-button.png?alt=media&token=5f3e240e-1b9d-41dc-92bb-aaf6baac24e2'
                                       })
             };
--- PilotCityRefactored/src/components/matchmaking/employer/mm_employer.vue
+++ PilotCityRefactored/src/components/matchmaking/employer/mm_employer.vue
@@ -145,32 +145,37 @@ export default {
       this.search_options.keys = [];
         // pull all the parameters from the filter, concatenate them into a string called 'key' //
         // only search for relavant keys based on the difference of params 
+      if (this.filtered_grades && this.filtered_grades.length)
+      {
+        key +=  String(this.filtered_grades);
+        this.search_options.keys.push("Grade");
+        key = key.replace(/th/g, '');
+        key = key.replace(/Grade/g, '');
+      }
       if (this.filtered_courses && this.filtered_courses.length){
         key += String(this.filtered_courses);
         // only course name is relavant in this case
         this.search_options.keys.push("coursename");  
       }
-      if (this.filtered_grades && this.filtered_grades.length)
-      {
-        key += " " + String(this.filtered_grades);
-        this.search_options.keys.push("Grade");
-      }
+      
       if (this.filtered_skills && this.filtered_skills.length)
       {
-         key += " " + String(this.filtered_skills);
+         key += String(this.filtered_skills);
         this.search_options.keys.push("selected_skills_keywords");
         this.search_options.keys.push("selected_industry_keywords");
       }
       if (this.filtered_locations && this.filtered_locations.length)
       {
-        key += " " + String(this.filtered_locations);
+        key += String(this.filtered_locations);
         this.search_options.keys.push("school_address");
         this.search_options.keys.push("school_district");
         this.search_options.keys.push("school_name");
       }
+      key = key.replace(/\s,/g, '')
       // if no params is selected from the filter, we return the whil array.
       if (key == "")
         return (this.loaded_classrooms);
+
       /* ======= Testing Purpose =======
           console.log(key);
           console.log(this.search_options.keys)
@@ -211,7 +216,7 @@ export default {
       //   str = str.replace(/\s/g, '+');
       //   arr.push(str);
       // }
-      var arr = _.filter(this.loaded_teachers, teacher => {return teacher.coordinate});
+      var arr = _.filter(this.filter_list, teacher => {return teacher.coordinate});
       return (arr);
     }
   },
@@ -314,6 +319,8 @@ export default {
                   class_data["school_name"]=self.findbyId(self.loaded_teachers,class_data.teacher_uid).school_name
                   class_data["selected_industry_keywords"]=self.findbyId(self.loaded_teachers,class_data.teacher_uid).selected_industry_keywords
                   class_data["selected_skills_keywords"]=self.findbyId(self.loaded_teachers,class_data.teacher_uid).selected_skills_keywords
+                   class_data["coordinate"]=self.findbyId(self.loaded_teachers,class_data.teacher_uid).coordinate;
+                  
                   // console.log(doc.data());
                   self.loaded_classrooms.push(class_data);
                 });
